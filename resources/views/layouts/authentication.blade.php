6180d199e7dcdf5461770c83ae48120d5268d633
AbrahamMudonge <abrahamsadler01@outlook.com>
2021-08-05 13:14:41 +0300

    admintemplate

---
+++ resources/views/layouts/authentication.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('includes.header')
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        @yield('authentication')
+
+    </div>
+
+  @include('includes.script')
+
+</body>
+
+</html>
